1ccf3bbcfcc1dec88ab9ac5b5a12b94203b71238
q8wahab <q8_xprt@hotmail.com>
2024-07-19 16:45:02 +0300

    update donation list schema to araay of object and add more to donation items and list

--- apis/donations/controllers.js
+++ apis/donations/controllers.js
@@ -32,6 +32,34 @@ const CreateList = async (req, res, next) => {
   }
 };
 
+const delOneList = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const dellist = await DonationList.findByIdAndDelete(id, req.body);
+    if (dellist) {
+      return res.status(201).json(dellist);
+    } else {
+      return res.status(404).json({ msg: "delete list faild!" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateOneList = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const updatedlist = await DonationList.findByIdAndUpdate(id, req.body);
+    if (updatedlist) {
+      return res.status(201).json(updatedlist);
+    } else {
+      return res.status(404).json({ msg: "update list faild!" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 const CreateDonation = async (req, res, next) => {
   try {
     if (req.file) {
@@ -51,9 +79,26 @@ const CreateDonation = async (req, res, next) => {
   }
 };
 
+const delOneDonationItem = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const deleteddonation = await DonationItem.findByIdAndDelete(id, req.body);
+    if (deleteddonation) {
+      return res.status(201).json(deleteddonation);
+    } else {
+      return res.status(404).json({ msg: "delete donation faild!" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllDonationsItems,
   CreateDonation,
   CreateList,
   getAllList,
+  delOneList,
+  updateOneList,
+  delOneDonationItem,
 };
--- apis/donations/routes.js
+++ apis/donations/routes.js
@@ -4,15 +4,22 @@ const {
   CreateDonation,
   CreateList,
   getAllList,
+  delOneList,
+  updateOneList,
+  delOneDonationItem,
 } = require("./controllers");
 const upload = require("../../middlewares/multer");
 
 const DonationRouter = express.Router();
 
 DonationRouter.get("/", getAllDonationsItems);
-DonationRouter.get("/list", getAllList);
-
 DonationRouter.post("/", upload.single("image"), CreateDonation);
+DonationRouter.delete("/:id", upload.single("image"), delOneDonationItem);
+
+DonationRouter.get("/list", getAllList);
+DonationRouter.delete("/list/:id", upload.single("image"), delOneList);
 DonationRouter.post("/list", upload.single("image"), CreateList);
+DonationRouter.delete("/list/:id", upload.single("image"), delOneList);
+DonationRouter.put("/list/:id", upload.single("image"), updateOneList);
 
 module.exports = DonationRouter;
